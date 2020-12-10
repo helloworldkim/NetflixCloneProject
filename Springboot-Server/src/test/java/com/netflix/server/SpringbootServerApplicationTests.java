8c257dbc28413ae4dbba54cada68cfdcf1fb06e1
helloworldkim <arinwater@gmail.com>
2020-12-10 10:42:36 +0900

    최초커밋

---
+++ Springboot-Server/src/test/java/com/netflix/server/SpringbootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
