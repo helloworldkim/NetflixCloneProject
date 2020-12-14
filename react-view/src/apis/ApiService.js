9d8f0e300f68e2524e4399c156a4b394e4aec7b4
qqueppue <efgdraw@naver.com>
2020-12-14 17:21:56 +0900

    api upload

--- react-view/src/apis/ApiService.js
+++ react-view/src/apis/ApiService.js
@@ -1,10 +1,59 @@
 import Axios from 'axios';
 
-const SPRING_API_URL = '';
+const API_URL = 'http://localhost:8080/api/'  //spring boot back-end url
 
 class ApiService {
-    fetchMovies(){
-        return Axios.get(SPRING_API_URL);
+
+    popularMovie(i) {   //인기있는 영화 데이터 받기
+        return Axios.get(API_URL + '/movie/popular?page=' + i);
+    }
+
+    searchMovie(i, keyword) {   //검색해서 영화 데이터받기
+        return Axios.get(API_URL + '/movie/detail?page=' + i + '&query=' + keyword);
+    }
+
+    detailMovie(id) {   //영화 디테일정보 받기
+        return Axios.get(API_URL + '/movie/search?id=' + id);
+    }
+
+    genreMovie(i, genre) {   //장르로 나눠서 영화 데이터받기
+        return Axios.get(API_URL + '/movie/genre?page=' + i + '&genre=' + genre);
+    }
+
+    sortByMovie(i, sort) {   //원하는 방식으로 정렬해서 영화데이터 받기
+        return Axios.get(API_URL + '/movie/sortBy?page=' + i + '&sort=' + sort);
+    }
+
+    imageMovie(id) {   //영화 이미지들 받기
+        return Axios.get(API_URL + '/movie/image?id=' + id);
+    }
+
+    realImageMovie(i, fileFath) {   //위에서 선택한 영화 이미지하나 불러올수 있는 주소 데이터 받기
+        return Axios.get(API_URL + '/movie/realimage?image=' + fileFath);
+    }
+
+    videoMovie(id) {   //영화 비디오에 사용될 유튜브 키값 받기
+        return Axios.get(API_URL + '/movie/video?id=' + id);
+    }
+
+    fetchUsers() {
+        return Axios.get(API_URL);
+    }
+
+    fetchUserByID(id) {  //'http://localhost:8090/users/2' -> 특정 사용자 정보조회
+        return Axios.get(API_URL + '/' + id)
+    }
+
+    addUser(user) { //POST 사용자 정보 back-end 전달
+        return Axios.post(API_URL, user);
+    }
+
+    editUser(user) {    //PUT 사용자 수정정보 back-end로 전달
+        return Axios.put(API_URL + '/' + user.id, user);
+    }
+
+    removeUser(id) {    //DELETE 사용자 아이디 전달
+        return Axios.delete(API_URL + '/' + id);
     }
 }
 
--- react-view/src/components/Main.js
+++ react-view/src/components/Main.js
@@ -1,37 +1,73 @@
-import React from "react";
-import Row from './Row';
+import React, { Component, useState } from "react";
 
-const MainPage = () => {
+import ApiService from '../apis/ApiService';
 
-    return (
-        <div style={{backgroundColor:'#181818'}}>
-            <div className='row'>
-                <div className='col-12'>
-                    <iframe
-                    src='https://youtube.com/embed/UIA1QoGATHY'
-                    title='Youtube Video Player'
-                    className='video'
-                    allowFullScreen
-                    style={{width:'100%', height: '80vh'}}                  
-                    />
+class MainPage extends Component {
+    handleGenre = async (i, genre) => {
+        await ApiService.genreMovie(i, genre)
+            .then(res => {
+                if(genre === 'Action' && 'Adventure') {
+                    
+                }
+                if(genre === 'Comedy') {
+
+                }
+                if(genre === 'Documentary') {
+
+                }
+                if(genre === 'Family') {
+
+                }
+                if(genre === 'Fantasy') {
+
+                }
+                if(genre === 'Horror') {
+
+                }
+            })
+            .catch(err => {
+                console.error('genreMovie get 오류 : ', err);
+                alert('영화 장르 불러오기 오류');
+            });
+    }
+
+//    selectGenre = () => {
+        //genres.name(장르), orininal_title(제목), overview(설명), release_date(개봉년월일), runtime
+
+ //   }
+
+    render() {
+
+        return (
+            <div style={{backgroundColor:'#181818'}}>
+                <div className='row'>
+                    <div className='col-12'>
+                        <iframe
+                        src='https://youtube.com/embed/UIA1QoGATHY'
+                        title='Youtube Video Player'
+                        className='video'
+                        allowFullScreen
+                        style={{width:'100%', height: '80vh'}}                  
+                        />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <button type="button" className="btn btn-light"
+                            style={{ margin: 10 }}
+                            children="▶ 재생" />
+                        <button type="button" className="btn btn-secondary"
+                            children="상세 정보" />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                        <a href='#'>{}</a>
+                    </div>
                 </div>
             </div>
-            <div className='row'>
-                <div className='col'>
-                    <button type="button" className="btn btn-light"
-                        style={{ margin: 10 }}
-                        children="▶ 재생" />
-                    <button type="button" className="btn btn-secondary"
-                        children="상세 정보" />
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col'>
-                    <Row />
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 
 }
 
