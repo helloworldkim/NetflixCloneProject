a5517f260ed2ae28d6fc51586ca608ed5418ad41
qqueppue <efgdraw@naver.com>
2020-12-16 17:05:30 +0900

    main tmdb 연동

--- react-view/src/apis/ApiService.js
+++ react-view/src/apis/ApiService.js
@@ -1,29 +1,29 @@
 import Axios from 'axios';
 
-const API_URL = 'http://localhost:8090/'  //spring boot back-end url
+const API_URL = 'http://localhost:8090/api'  //spring boot back-end url
 
 class ApiService {
 
     popularMovie(i) {   //인기있는 영화 데이터 받기
-        return Axios.get(API_URL + 'api/movie/popular?page=' + i);
+        return Axios.get(API_URL + '/movie/popular?page=' + i);
     }
 
     searchMovie(i, keyword) {   //검색해서 영화 데이터받기
-        return Axios.get(API_URL + 'api/movie/detail?page=' + i + '&query=' + keyword);
+        return Axios.get(API_URL + '/movie/detail?page=' + i + '&query=' + keyword);
     }
 
     detailMovie(id) {   //영화 디테일정보 받기
-        return Axios.get(API_URL + 'api/movie/search?id=' + id);
+        return Axios.get(API_URL + '/movie/search?id=' + id);
     }
 
     genreMovie(i, genre) {   //장르로 나눠서 영화 데이터받기
         var result;
-        var all_url = API_URL + 'api/movie/genre?page=' + i + '&genre=' + genre;
+        var all_url = API_URL + '/movie/genre?page=' + i + '&genre=' + genre;
         return Axios.get(all_url);
     }
 
     sortByMovie(i, sort) {   //원하는 방식으로 정렬해서 영화데이터 받기
-        return Axios.get(API_URL + 'api/movie/sortBy?page=' + i + '&sort=' + sort);
+        return Axios.get(API_URL + '/movie/sortBy?page=' + i + '&sort=' + sort);
     }
 
     // imageMovie(id) {   //영화 이미지들 받기
@@ -31,21 +31,23 @@ class ApiService {
     // }
 
     realImageMovie(i, fileFath) {   //위에서 선택한 영화 이미지하나 불러올수 있는 주소 데이터 받기
-        return Axios.get(API_URL + 'api/movie/realimage?image=' + fileFath);
+        return Axios.get(API_URL + '/movie/realimage?image=' + fileFath);
     }
 
     videoMovie(id) {   //영화 비디오에 사용될 유튜브 키값 받기
-        return Axios.get(API_URL + 'api/movie/video?id=' + id);
+        return Axios.get(API_URL + '/movie/video?id=' + id);
     }
 
-    loginUser() {//로그인
-        return Axios.get(API_URL + 'user/login')
+    fetchUsers() {
+        return Axios.get(API_URL);
     }
-    addUser() {//회원가입
-        return Axios.get(API_URL +'user')
+
+    fetchUserByID(id) {  //'http://localhost:8090/users/2' -> 특정 사용자 정보조회
+        return Axios.get(API_URL + '/' + id)
     }
-    checkEmail(email) {//email 중복확인
-        return Axios.get(API_URL + 'user?email='+email);
+
+    addUser(user) { //POST 사용자 정보 back-end 전달
+        return Axios.post(API_URL, user);
     }
 
 }
--- react-view/src/components/Main.js
+++ react-view/src/components/Main.js
@@ -1,72 +1,94 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
+import Fade from 'react-reveal';
+// import Slider from 'react-slick';
 
-import ApiService from '../apis/ApiService';
+//import ApiService from '../apis/ApiService';
+import tmdbService from '../apis/tmdbService';
+import Movie from "./Movie";
 
 class MainPage extends Component {
     constructor(props) {
         super(props);
         
         this.state = {
-            genre: ['Action', 'Adventure', 'Comedy', 'Documentary', 'Family', 'Fantasy', 'Horror'],
+            genre: ['28', '12', '35', '99', '10751', '14', '27'],
+            //genre: ['Action', 'Adventure', 'Comedy', 'Documentary', 'Family', 'Fantasy', 'Horror'],
+            // populars: [],
+
             result: {},
+            actions: [],
+            adventures: [],
+            comedy: [],
+            documentary: [],
+            family: [],
+            fantasy: [],
+            horrors: [],
         }
     }
-    
-    async handleGenre(i, genre) {
-        await ApiService.genreMovie(i, genre)
-        .then(res => {
-                this.setState({ result: res.results }); 
-                console.log("TEST1");
-            })
-        .catch(err => {
-                console.error('genreMovie get 오류 : ', err);
-                alert('영화 장르 불러오기 오류');
-                return;
-            });
-    }
 
-    handlePopular(i) {
-        ApiService.popularMovie(i)
-            .then(res => {
-                console.log('인기영화 불러오기 성공!');
-            })
-            .catch(err => {
-                console.error('handlePopular 오류:', err);
-                alert('인기영화를 불러오지 못했습니다. \n 관리자에게 문의하세요!');
-            });
-    }
+    // popularMovie() {
+    //     tmdbService.getPopular()
+    //         .then(res => {
+    //             this.setState({ populars: res.data });
+    //         })
+    //         .catch(err => {
+    //             console.error('popularMovie get 오류 : ', err);
+    //             alert('인기영화 불러오기 오류');
+    //         });
+    // }
 
-    handleVideo(id) {
-        ApiService.videoMovie(id)
-            .then(res => {
-                console.log('인기영화 불러오기 성공!');
-            })
-            .catch(err => {
-                console.error('handleVideo 오류:',err);
-                alert('동영상을 불러오지 못했습니다. \n 관리자에게 문의하세요!');
-            });
-    }
-
-//    selectGenre = () => {
-        //genres.name(장르), orininal_title(제목), overview(설명), release_date(개봉년월일), runtime
-
- //   }
-
-    componentDidMount() {
-        for (var i= 0; i < this.state.genre.length; i++) {
-            //console.log(this.state.genre[i]);
-            // handleGenre
+    async componentDidMount() {
+        for (var i = 0; i < this.state.genre.length; i++) {
             //debugger;
-            this.handleGenre(1, this.state.genre[i]);
-   
+            await tmdbService.getGenreList(this.state.genre[i])
+                .then(res => {
+                    //console.log(res.data.results);
+                    switch (i) {
+                        case 0: // 액션
+                            this.setState({actions :res.data.results.slice(10)});
+                            break;
+                        case 1: // 어드벤처                    
+                            this.setState({adventures :res.data.results.slice(10)});
+                            break;
+                        case 2: // 코미디
+                            this.setState({comedy :res.data.results.slice(10)});
+                            break;
+                        case 3: // 다큐멘터리
+                            this.setState({documentary :res.data.results.slice(10)});
+                            break;
+                        case 4: // 가족 영화
+                            this.setState({family :res.data.results.slice(10)});
+                            break;
+                        case 5: // 판타지
+                            this.setState({fantasy :res.data.results.slice(10)});
+                            break;
+                        case 6: // 호러
+                            this.setState({horrors :res.data.results.slice(10)});
+                            break;
+                        default:
+                            break;
+                    }
+                })
+                .catch(err => {
+                    console.error('getGenreList get 오류 : ', err);
+                    alert('영화 장르 불러오기 오류');
+                });
         }
-        this.handlePopular(1);
 
     }
-
-
 
     render() {
+
+        // const settings = {  // 영화 슬라이더 세팅
+        //     dots: true,
+        //     slidesToShow: 3,
+        //     slidesToScroll: 3,
+        //     infinite: true,
+        //     speed: 500,
+        //     // rows: 5,
+        //     // slidesPerRow: 5
+        // }
+
         return (
             <div style={{backgroundColor:'#181818'}}>
                 <div className='row'>
@@ -89,11 +111,89 @@ class MainPage extends Component {
                             children="상세 정보" />
                     </div>
                 </div>
-                <div className='row'>
-                    <div className='col'>
-                        <a href='#'>{}</a>
+                {/* <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>인기 영화</h2>
+                            {this.state.populars.map((populars, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + populars.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
                     </div>
-                </div>
+                </Fade> */}
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>액션</h2>
+                            {/* <Slider {...settings}> */}
+                                {this.state.actions.map((movie, index) => {
+                                    console.log('data : ', movie);
+                                    return <Movie key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} movie={movie} width="190" alt='' />
+                                })}
+                            {/* </Slider> */}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>어드벤쳐</h2>
+                            {this.state.adventures.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>코미디</h2>
+                            {this.state.comedy.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>다큐멘터리</h2>
+                            {this.state.documentary.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>가족 영화</h2>
+                            {this.state.family.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>판타지</h2>
+                            {this.state.fantasy.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
+                <Fade bottom>
+                    <div className='row'>
+                        <div className='col'>
+                            <h2 className='genrefont'>공포</h2>
+                            {this.state.horrors.map((movie, index) => {
+                                return <img key={index} src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} width="190" alt='' />
+                            })}
+                        </div>
+                    </div>
+                </Fade>
             </div>
         );
     }
