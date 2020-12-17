57086547f478b5d90db939f39aae68f7352e8322
helloworldkim <arinwater@gmail.com>
2020-12-17 15:10:40 +0900

    frontendev랑 apiservice 다시 merge해서 합쳤음 commit

--- react-view/src/route/Router.js
+++ react-view/src/route/Router.js
@@ -8,23 +8,23 @@ import Main from '../components/Main';
 import NewContent from '../components/NewContent';
 
 class Router extends Component {
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div>
-                        <Switch>
-                            <Route exact path='/main' component={Main} />
-                            <Route path='/login' component={Login} />
-                            <Route path='/join' component={Join} />
-                            <Route path='/myContent' component={Favorite} />
-                            <Route path='/newContent' component={NewContent} />
-                        </Switch>
-                    </div>
-                </BrowserRouter>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <Switch>
+              <Route exact path="/" component={Main} />
+              <Route path="/login" component={Login} />
+              <Route path="/join" component={Join} />
+              <Route path="/myContent" component={Favorite} />
+              <Route path="/newContent" component={NewContent} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default Router;
